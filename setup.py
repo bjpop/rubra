c95c6478150ba67422f9fb432c09098ef1da2c0b
Bernie Pope <florbitous@gmail.com>
2013-04-10 17:20:55 +1000

    Bumped version number to 0.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='Rubra',
-    version='0.1.2',
+    version='0.1.3',
     author='Bernie Pope',
     author_email='bjpope@unimelb.edu.au',
     packages=['rubra'],
